f7f1c89f432b923e08deae91d1a8a9089a4a57de
trotha01 <trevor.rothaus@sendgrid.com>
2016-05-14 12:23:30 -0700

    elm-format

--- Ports.elm
+++ Ports.elm
@@ -1,8 +1,10 @@
 port module Ports exposing (..)
 
+
 port playSoundOut : String -> Cmd msg
+
 
 port soundData : (List Int -> msg) -> Sub msg
 
-port logExternalOut : String -> Cmd msg
 
+port logExternalOut : String -> Cmd msg
--- WebAudio.elm
+++ WebAudio.elm
@@ -8,74 +8,111 @@ import Color exposing (blue)
 import Element exposing (Element, toHtml)
 import Ports exposing (playSoundOut, logExternalOut)
 
+
 -- MODEL
-type alias Model = List Int
+
+
+type alias Model =
+    List Int
+
 
 model : Model
-model = []
+model =
+    []
+
+
 
 -- UPDATE
+
+
 type Msg
-  = PlaySound
-  | SoundData (List Int)
+    = PlaySound
+    | SoundData (List Int)
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    PlaySound ->
-      ( model, (playSoundOut "" ))
-    SoundData data ->
-      (model++data, Cmd.none)
+    case msg of
+        PlaySound ->
+            ( model, (playSoundOut "") )
+
+        SoundData data ->
+            ( model ++ data, Cmd.none )
+
+
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
-  Html.div []
-  [ playButton
-  , toHtml (graph model) ]
+    Html.div []
+        [ playButton
+        , toHtml (graph model)
+        ]
+
 
 playButton : Html Msg
 playButton =
-  Html.button
-    [ onClick PlaySound ] [ text "Play" ]
+    Html.button [ onClick PlaySound ]
+        [ text "Play" ]
 
-graphWidth = 500
-graphHeight = 500
-halfGraphWidth = graphWidth / 2
+
+graphWidth =
+    500
+
+
+graphHeight =
+    500
+
+
+halfGraphWidth =
+    graphWidth / 2
+
 
 graph : Model -> Element
 graph model =
-  let points = List.length model
-      moveLeft =
-        if
-          points < graphWidth
-        then
-          0
-        else
-          graphWidth - points
-  in collage graphWidth graphHeight
-    [ group (List.indexedMap point model)
-        |> move (toFloat moveLeft, 0)
-    ]
+    let
+        points =
+            List.length model
+
+        moveLeft =
+            if points < graphWidth then
+                0
+            else
+                graphWidth - points
+    in
+        collage graphWidth
+            graphHeight
+            [ group (List.indexedMap point model)
+                |> move ( toFloat moveLeft, 0 )
+            ]
+
 
 point x y =
     circle 1
-      |> outlined (solid blue)
-      |> move (((toFloat x) - halfGraphWidth), toFloat (y-100))
+        |> outlined (solid blue)
+        |> move ( ((toFloat x) - halfGraphWidth), toFloat (y - 100) )
+
+
 
 -- SUBSCRIPTIONS
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Ports.soundData SoundData
+    Ports.soundData SoundData
+
+
 
 -- MAIN
 
+
 main : Program Never
 main =
-  Html.App.program
-    { init = ( model, Cmd.none )
-    , update = update
-    , view = view
-    , subscriptions = subscriptions
-    }
-
+    Html.App.program
+        { init = ( model, Cmd.none )
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
